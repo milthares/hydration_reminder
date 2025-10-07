60e8e3f0adb9c2e2877fa10db53eae606864f1ad
milthares <mandrea.business@gmail.com>
2025-10-07 15:33:26 +0200

    UI + reminder + fixes

--- hydration_reminder.py
+++ hydration_reminder.py
@@ -1,63 +1,65 @@
 # pyinstaller --onefile hydration_reminder.py --noconsole --hidden-import plyer.platforms.win.notification
-
-# Import tkinter
+# Imports
 from tkinter import *
-# Import time
 from datetime import datetime
 from time import *
-# Import plyer
 from plyer import *
-
+from PIL import *
 import pytz
-
+# Globals
+is_reminding = False
+# WindowSettings
 root = Tk()
-
-# root window title
 root.title("Hydration Reminder")
-
-drink_remainder = 10
-drink_timer = False
-# root geometry
-# root.geometry('800x600')
+root.geometry("250x250")
+# BakcgroundImage
+bg = PhotoImage(file="assets\\HydrationApp_background.png")
+# Canvas
+canvas = Canvas(root, width=250, height=250)
+canvas.pack(fill="both", expand=True)
+# DisplayImage
+canvas.create_image(0, 0, image=bg, anchor="nw")
+# Text
+text_id = canvas.create_text(125, 10, text = "Loading...")
+# Radios
+reminder_timer = IntVar()
+time_one = Radiobutton(root, text="15", variable=reminder_timer, value=15)
+time_two = Radiobutton(root, text="20", variable=reminder_timer, value=20)
+time_three = Radiobutton(root, text="30", variable=reminder_timer, value=30)
+# DisplayRadios
+radio1_canvas = canvas.create_window(40, 50, anchor="nw", window=time_one)
+radio2_canvas = canvas.create_window(80, 50, anchor="nw", window=time_two)
+radio3_canvas = canvas.create_window(120, 50, anchor="nw", window=time_three)
 # update function
 def update_app():
-        global drink_timer
-        global drink_remainder
-        italy_timezone = datetime.now(pytz.timezone('Europe/Rome'))
-        philippines_timezone = datetime.now(pytz.timezone('Asia/Manila'))
-        italy_time = italy_timezone.strftime('%H:%M:%S')
-        philippines_time = philippines_timezone.strftime('%H:%M:%S')
-        italy_lbl.config(text=italy_time)
-        philippines_lbl.config(text=philippines_time)
-        if drink_timer and drink_remainder > 0:
-                drink_remainder -= 1
-        elif drink_timer and drink_remainder == 0:
-                drink_warning()
-        root.after(1000, update_app)
-# function drink
-def drink():
-        global drink_timer
-        global drink_remainder
-        drink_timer = True
-        if drink_remainder < 10:
-                drink_remainder = 10
-def drink_warning():
-        global drink_timer
-        global toast
-        drink_timer = False
-        notification.notify(
-                title = 'Test',
-                message = 'Message',
-                app_icon = None,
-                timeout = 5,
-        )
-# adding label
-italy_lbl = Label(root)
-philippines_lbl = Label(root)
-italy_lbl.grid(column=0, row=0)
-philippines_lbl.grid(column=2, row=0)
-# button for drink
-drink_btn = Button(root, text="Drink!", command=drink)
-drink_btn.grid(column=1, row=1)
+  global is_reminding
+  global reminder_timer
+  timezone = datetime.now(pytz.timezone('Europe/Rome'))
+  time = timezone.strftime('%H:%M:%S')
+  # trigger notification based on time chosen
+  # lbl.config(text=time)
+  canvas.itemconfig(text_id, text=time)
+  print(is_reminding, reminder_timer.get())
+  match reminder_timer.get():
+    case 15 | 20 | 30:
+      if timezone.minute == 0 or (timezone.minute % reminder_timer.get()) == 0:
+        reminder()
+      else:
+        is_reminding = False
+  root.after(1000, update_app)
+# reminder
+def reminder():
+  global is_reminding
+  global toast
+  if not is_reminding:
+    is_reminding = True
+    notification.notify(
+      title = 'Test',
+      message = 'Message',
+      app_icon = None,
+      timeout = 5,
+    )
+  else:
+    pass
 update_app()
 mainloop()
